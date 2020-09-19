1a44ffa53786eac63848028e10171140506ca8a4
Mike Golubitsky <mike617@gmail.com>
2020-09-19 15:54:24 -0400

    Updates

---
+++ precache-manifest.792d0fc27b620c0be3ed30e088388864.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea099c600f7c3abdde749663ca779f94",
+    "url": "/music/index.html"
+  },
+  {
+    "revision": "9fcf82f0c376cf1b4c1a",
+    "url": "/music/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2a1a5545fc4cee21d1b4",
+    "url": "/music/static/js/2.af8aeee9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/music/static/js/2.af8aeee9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fcf82f0c376cf1b4c1a",
+    "url": "/music/static/js/main.1384a9e8.chunk.js"
+  },
+  {
+    "revision": "3ac88e11f787f57f2f24",
+    "url": "/music/static/js/runtime-main.01faecc1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/music/static/media/logo.5d5d9eef.svg"
+  }
+]);
