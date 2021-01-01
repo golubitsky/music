cf547031fab121911b973eddcf2c9e73a85f2434
Mike Golubitsky <mike617@gmail.com>
2020-12-31 22:54:47 -0500

    onClick: shuffle notes

--- src/App.js
+++ src/App.js
@@ -2,18 +2,30 @@ import React from "react";
 import "./App.css";
 import { randomNotes } from "./foundation/randomNoteGenerator.js";
 
-function App() {
-  return (
-    <div className="App">
-      <div className="grid">
-        {randomNotes().map((note, index) => (
-          <div className="cell" key={index}>
-            <p>{note}</p>
-          </div>
-        ))}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { notes: randomNotes() };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div
+          className="grid"
+          onClick={() => {
+            this.setState({ notes: randomNotes() });
+          }}
+        >
+          {this.state.notes.map((note, index) => (
+            <div className="cell" key={index}>
+              <p>{note}</p>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
