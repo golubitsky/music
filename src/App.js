0e775f5337aabe0290eab8f1404d882600a4b198
Mike Golubitsky <mike617@gmail.com>
2021-01-01 15:22:29 -0500

    refactor NotesInRandomOrder to its own screen

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import React from "react";
 import "./App.css";
-import { randomNotes } from "./foundation/randomNoteGenerator.js";
+import { NotesInRandomOrder } from "./screens/NotesInRandomOrder.js";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { notes: randomNotes() };
-  }
-
   render() {
     return (
       <div className="App">
-        <div
-          className="grid"
-          onClick={() => {
-            this.setState({ notes: randomNotes() });
-          }}
-        >
-          {this.state.notes.map((note, index) => (
-            <div className="cell" key={index}>
-              <p>{note}</p>
-            </div>
-          ))}
-        </div>
+        <NotesInRandomOrder />
       </div>
     );
   }
---
+++ src/screens/NotesInRandomOrder.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./NotesInRandomOrder.css";
+import { randomNotes } from "../foundation/randomNoteGenerator.js";
+
+export class NotesInRandomOrder extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { notes: randomNotes() };
+  }
+
+  render() {
+    return (
+      <div className="NotesInRandomOrder">
+        <div
+          className="grid"
+          onClick={() => {
+            this.setState({ notes: randomNotes() });
+          }}
+        >
+          {this.state.notes.map((note, index) => (
+            <div className="cell" key={index}>
+              <p>{note}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
