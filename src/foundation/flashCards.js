6358f4fb847bcc137b3dc9b0e0ae81616e835fe2
Mike Golubitsky <mike617@gmail.com>
2021-01-01 15:06:23 -0500

    two flipped flashcards for each set of content

--- src/foundation/flashCards.js
+++ src/foundation/flashCards.js
@@ -3,11 +3,17 @@ const _ = require("lodash");
 import flashCards from "./flashCards/polychordFractions.json";
 
 function cards() {
-  return _.entries(flashCards).map(function (cardValues) {
-    return {
-      front: cardValues[0],
-      back: cardValues[1],
-    };
+  return _.entries(flashCards).flatMap(function (cardValues) {
+    return [
+      {
+        front: cardValues[0],
+        back: cardValues[1],
+      },
+      {
+        front: cardValues[1],
+        back: cardValues[0],
+      },
+    ];
   });
 }
 
