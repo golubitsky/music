57d7d66678220584acf6094ece0c76b0886f7550
Mike Golubitsky <mike617@gmail.com>
2021-01-01 22:56:15 -0500

    Updates

--- precache-manifest.aad933df297ca8ddd14f4df3d091faf2.js
+++ precache-manifest.aad933df297ca8ddd14f4df3d091faf2.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fe717467d70ba57f946dd03ce58e0c4f",
+    "revision": "ab0861b2b73a2ab659c69c1cf8681357",
     "url": "/music/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/music/static/css/2.2a86faee.chunk.css"
   },
   {
-    "revision": "1adc1f74e4a177ef6975",
+    "revision": "f999f016b89ed0d4867b",
     "url": "/music/static/css/main.27c49b2a.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/music/static/js/2.f3ffbfc5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1adc1f74e4a177ef6975",
-    "url": "/music/static/js/main.9ee10b8d.chunk.js"
+    "revision": "f999f016b89ed0d4867b",
+    "url": "/music/static/js/main.3b91c082.chunk.js"
   },
   {
     "revision": "3ac88e11f787f57f2f24",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/music/precache-manifest.6792faa86a9fef9a0133c411a809c848.js"
+  "/music/precache-manifest.aad933df297ca8ddd14f4df3d091faf2.js"
 );
 
 self.addEventListener('message', (event) => {
