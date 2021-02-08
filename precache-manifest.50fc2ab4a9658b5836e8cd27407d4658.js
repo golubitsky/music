5693fb6b99d0698f9c4e9a2f012bb593bddd37e5
golubitsky <golubitsky@users.noreply.github.com>
2021-02-08 02:56:37 +0000

    Deploying to gh-pages from  @ 566480989689716473ec293da597d14ed9c739df 🚀

--- precache-manifest.50fc2ab4a9658b5836e8cd27407d4658.js
+++ precache-manifest.50fc2ab4a9658b5836e8cd27407d4658.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b1e81b4d1201f8e875d0ca1a144dbcf0",
+    "revision": "b0bc32d29259fef541aa5038b0f0a9e7",
     "url": "/music/index.html"
   },
   {
-    "revision": "63aaf425c16fa99348fb",
+    "revision": "17abd9aa1914be841528",
     "url": "/music/static/css/2.2a86faee.chunk.css"
   },
   {
-    "revision": "d755e7753cc7e4491b8a",
+    "revision": "b1ff938c176b7db45bd9",
     "url": "/music/static/css/main.73a3db79.chunk.css"
   },
   {
-    "revision": "63aaf425c16fa99348fb",
-    "url": "/music/static/js/2.588ed7f7.chunk.js"
+    "revision": "17abd9aa1914be841528",
+    "url": "/music/static/js/2.ca3c56d2.chunk.js"
   },
   {
     "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
-    "url": "/music/static/js/2.588ed7f7.chunk.js.LICENSE.txt"
+    "url": "/music/static/js/2.ca3c56d2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d755e7753cc7e4491b8a",
-    "url": "/music/static/js/main.d3dbb0c6.chunk.js"
+    "revision": "b1ff938c176b7db45bd9",
+    "url": "/music/static/js/main.1a3e6cf0.chunk.js"
   },
   {
     "revision": "3ac88e11f787f57f2f24",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/music/precache-manifest.df81970fb0834d99457d7fde8468be2a.js"
+  "/music/precache-manifest.50fc2ab4a9658b5836e8cd27407d4658.js"
 );
 
 self.addEventListener('message', (event) => {
